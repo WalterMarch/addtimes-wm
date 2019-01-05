94027383abb7fdfcc4b2dffde28bee83c7d6c84f
Walter March <walter.march@accolade.com>
2019-01-05 14:25:51 -0500

    pep8 - variable and function names

--- addtimes.py
+++ addtimes.py
@@ -1,44 +1,44 @@
 import sys
 
 
-def addtime(thistotal, thisin):
+def add_time(this_total, this_in):
     # time string should be checked
     
     # split and set up time lists
-    totnums = thistotal.split(":")
-    innums = thisin.split(":")
+    tot_nums = this_total.split(":")
+    in_nums = this_in.split(":")
 
-    if len(totnums) != len(innums):
-        if len(totnums) < len(innums):
-            totnums.insert(0, "00")
+    if len(tot_nums) != len(in_nums):
+        if len(tot_nums) < len(in_nums):
+            tot_nums.insert(0, "00")
         else:
-            innums.insert(0, "00")
+            in_nums.insert(0, "00")
 
-    for i in range(0, (-1 * len(innums)), -1):
-        slicetotal = int(totnums[i - 1]) + int(innums[i - 1]) 
+    for i in range(0, (-1 * len(in_nums)), -1):
+        slice_total = int(tot_nums[i - 1]) + int(in_nums[i - 1])
         
-        if (slicetotal > 60) and ((i - 1) != -3):
-            slicetotal = slicetotal % 60
-            totnums[i - 2] = int(totnums[i - 2]) + (slicetotal // 60) + 1
+        if (slice_total > 60) and ((i - 1) != -3):
+            slice_total = slice_total % 60
+            tot_nums[i - 2] = int(tot_nums[i - 2]) + (slice_total // 60) + 1
         
-        totnums[i - 1] = str(slicetotal).zfill(2)
+        tot_nums[i - 1] = str(slice_total).zfill(2)
     
-    newtotal = ":".join(totnums)
+    new_total = ":".join(tot_nums)
 
-    return newtotal
+    return new_total
 
 
-myin = "x"
+my_in = "x"
 total = "00:00:00"
 
-while myin != "0":
-    mystr = "Enter HH:MM:SS or MM:SS; 0 to end: "
+while my_in != "0":
+    my_str = "Enter HH:MM:SS or MM:SS; 0 to end: "
 
     if sys.version_info[0] >= 3:
-        myin = input(mystr)
+        my_in = input(my_str)
     else:
-        myin = raw_input(mystr)
+        my_in = raw_input(my_str)
 
-    if myin != "0":
-        total = addtime(total, myin)
+    if my_in != "0":
+        total = add_time(total, my_in)
         print(total)
